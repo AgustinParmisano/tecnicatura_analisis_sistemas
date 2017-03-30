b03a295134faaf9a4ab417d9de41b86f4868780b
Agustin Parmisano <agustinparmisano@gmail.com>
2017-03-29 21:48:11 -0300

    Agregando ejercico de strings complejo

---
+++ práctica1/complejo.py
@@ -0,0 +1,6 @@
+parrafo = """En Python es posible
+armar un parrafo
+muy sencillamente"""
+complejo = parrafo[3:9] + " " + parrafo[10:12] + " " + parrafo[-13:] + " " + parrafo[-17:-13] + " " + parrafo[13:20]
+print(parrafo)
+print(complejo)
