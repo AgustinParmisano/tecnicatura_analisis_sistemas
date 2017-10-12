8412e645312265a319b59bbc11aa7cf0a7dff6a6
agustinparmisano <agustinparmisano@gmail.com>
2017-10-12 20:45:47 -0300

    Agregando posts para estadisticas (dia, mes hora y entre fechas con parametros de devices opcionales)

--- trabajos_entregas/estacion_meteo/ubidots/ubitest.py
+++ trabajos_entregas/estacion_meteo/ubidots/ubitest.py
@@ -1,13 +1,17 @@
 from ubidots import ApiClient
+import random
+import time
 
-mi_token = '' # id del token que del cliente ubidots
+mi_token = 'A1E-23UQYfbsc0yT8Bg3jzYntlvaBYiIDF' # id del token que del cliente ubidots
 
 api = ApiClient(token=mi_token)
 
-mi_variable = '' # id de la variable del device de ubidots
+id_variable = '59df9065c03f971fb1aea2ea' # id de la variable del device de ubidots: temperatura mauro: 59df9065c03f971fb1aea2ea
 
-variable = api.get_variable(mi_variable)
+obj_variable = api.get_variable(id_variable)
 
-
-valor = '10' # el valor que deseamos enviarle a la variable de ubidots
-nuevo_valor = mi_variable.save_value({'value':valor})
+while True:
+	valor = random.randint(0,101) # el valor que deseamos enviarle a la variable de ubidots
+	nuevo_valor = obj_variable.save_value({'value':valor})
+	time.sleep(5)
+	print "Enviando dato: " + str(valor)
